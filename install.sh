47ddcea0a7358a030f7d401bbe43cb4911c2b580
Staubgeborener <eric.schroeder@protonmail.com>
2023-04-01 02:39:30 +0200

    change paths in install script

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ LatestVersion=$(curl -Lsk 'https://github.com/Staubgeborener/klipper-backup/raw/
 
 if [[ $LatestVersion > $LocalVersion ]] ; then
     echo "New version $LatestVersion released! Start update"
-    wget https://github.com/Staubgeborener/klipper-backup/releases/download/$LatestVersion/files.zip
-    unzip -o files.zip
-    cp -R ./files/* $pwd
-    rm -rf files.zip files
+    wget https://github.com/Staubgeborener/klipper-backup/releases/download/$LatestVersion/klipper-backup-main.zip
+    unzip -o klipper-backup-main.zip
+    cp -R ./klipper-backup-main/* $pwd
+    rm -rf klipper-backup-main.zip klipper-backup-main
     chmod +x *.sh
 else
     echo "You are up-to-date"
