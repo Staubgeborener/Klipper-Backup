9b1d09a069eac43a7cc947efd09147f16e39563e
Staubgeborener <eric.schroeder@protonmail.com>
2023-04-16 10:15:33 +0200

    better coding standard

--- script.sh
+++ script.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 
 # Set parent directory path
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+parent_path=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
 
 # Initialize variables from .env file
-github_token=`grep 'github_token=' $parent_path/.env | sed 's/^.*=//'`
-github_username=`grep 'github_username=' $parent_path/.env | sed 's/^.*=//'`
-github_repository=`grep 'github_repository=' $parent_path/.env | sed 's/^.*=//'`
-backup_folder=`grep 'backup_folder=' $parent_path/.env | sed 's/^.*=//'`
+github_token=$(grep 'github_token=' "$parent_path"/.env | sed 's/^.*=//')
+github_username=$(grep 'github_username=' "$parent_path"/.env | sed 's/^.*=//')
+github_repository=$(grep 'github_repository=' "$parent_path"/.env | sed 's/^.*=//')
+backup_folder=$(grep 'backup_folder=' "$parent_path"/.env | sed 's/^.*=//')
 
 # Change directory to parent path
-cd $parent_path
+cd "$parent_path" || exit
 
 # Check if backup folder exists, create one if it does not
 if [ ! -d "$parent_path/$backup_folder" ]; then
-  mkdir $parent_path/$backup_folder
+  mkdir "$parent_path/$backup_folder"
 fi
 
 # Copy important files into backup folder
-cp $(grep 'path_' $parent_path/.env | sed 's/^.*=//') $parent_path/$(grep 'backup_folder=' $parent_path/.env | sed 's/^.*=//')
+while read -r path; do
+  file=$(basename "$path")
+  cp "$path" "$parent_path/$backup_folder/$file"
+done < <(grep 'path_' "$parent_path"/.env | sed 's/^.*=//')
 
 # Git commands
 git init
-git rm -rf --cached $parent_path/.env
-git add $parent_path
-git commit -m "new backup from $(date +"%d-%m-%y")"
+git rm -rf --cached "$parent_path"/.env
+git add "$parent_path"
+git commit -m "New backup from $(date +"%d-%m-%y")"
 git push https://"$github_token"@github.com/"$github_username"/"$github_repository".git
