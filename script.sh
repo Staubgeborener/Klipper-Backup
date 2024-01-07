8964dfa85f95cec024a757bc19a740804abb4098
Tyler's Neptune 4 Pro <mks@mkspi.local.lan>
2024-01-07 15:50:57 -0700

    update url

--- script.sh
+++ script.sh
@@ -34,7 +34,7 @@ done < <(grep -v '^#' "$parent_path/.env" | grep 'path_' | sed 's/^.*=//')
 
 # Add basic readme to backup repo
 backup_parent_directory=$(dirname "$backup_folder")
-echo -e "# klipper-backup 💾 \nKlipper backup script for manual or automated GitHub backups \n\nThis backup is provided by [klipper-backup](https://github.com/Staubgeborener/klipper-backup)." > "$HOME/$backup_parent_directory/README.md"
+echo -e "# klipper-backup 💾 \nKlipper backup script for manual or automated GitHub backups \n\nThis backup is provided by [klipper-backup](https://github.com/tylerjet/klipper-backup)." > "$HOME/$backup_parent_directory/README.md"
 
 # Individual commit message, if no parameter is set, use the current timestamp as commit message
 timezone=$(timedatectl | grep "Time zone" | awk '{print $3}')
