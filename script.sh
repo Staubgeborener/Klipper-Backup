9759957d9c5d4780e3f3f74d46edcf30f8fcb759
MrTylerjet <MrTylerjet@gmail.com>
2024-01-13 01:19:04 -0700

    Add check for if  and exit while informing the user that the backup folder cannot be the root directory of /home/{username}/, created a backup_path variable within script.sh as there were 5 plus areas where the same  was being used so having a variable that you only need to change once to change all of them seems better, removed a random white space, as well added -f to git push ensuring that any push is forced, *this removes some errors about failing to push refs to your repository due to the remote containing files that you don't have locally which we can ignore, added some additional checks for proper file and folder removal after the backup is pushed ensuring that most situations or folder design is handled.

--- script.sh
+++ script.sh
@@ -10,12 +10,22 @@ parent_path=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
 # Initialize variables from .env file
 source "$parent_path"/.env
 
+# Check that backup_folder is not set to root of users home directory
+backup_folderDepth=$(echo "$backup_folder" | tr '/' '\n' | grep -c .)
+if [[ $backup_folder == '.' ]]; then
+echo "Your \$backup_folder path cannot be the root of: $HOME"
+echo "Please change the path location in .env!"
+exit 1
+fi
+
 # Change directory to parent path
 cd "$parent_path" || exit
 
+backup_path="$HOME/$backup_folder"
+
 # Check if backup folder exists, create one if it does not
-if [ ! -d "$HOME/$backup_folder" ]; then
-  mkdir -p "$HOME/$backup_folder"
+if [ ! -d $backup_path ]; then
+  mkdir -p $backup_path
 fi
 
 while IFS= read -r path; do
@@ -28,14 +38,22 @@ while IFS= read -r path; do
     elif [[ $(basename "$file") =~ ^printer-[0-9]+_[0-9]+\.cfg$ ]]; then
         echo "Skipping file: $file"
     else
-      cp -r $file $HOME/$backup_folder/
+      cp -r $file $backup_path/
     fi
   done
 done < <(grep -v '^#' "$parent_path/.env" | grep 'path_' | sed 's/^.*=//')
 
-# Add basic readme to backup repo
-backup_parent_directory=$(dirname "$backup_folder")
+# Check if backup_folder path depth is greater than 1, this is needed to ensure that if path is only 1 level deep we do not create the git repo in /home/{username}/
+# we DO NOT want to use backup_path here as we want to exclude /home/{username}/ from the awk search
+if [[ $backup_folderDepth -gt 1 ]]; then
+backup_parent_directory=$(echo "$backup_folder" | awk -F'/' '{print $1}') # first level of backup path
+else
+backup_parent_directory=$backup_folder
+fi
+
 cp "$parent_path"/.gitignore "$HOME/$backup_parent_directory/.gitignore"
+
+# Create and add Readme to backup folder
 echo -e "# klipper-backup 💾 \nKlipper backup script for manual or automated GitHub backups \n\nThis backup is provided by [klipper-backup](https://github.com/Staubgeborener/klipper-backup)." > "$HOME/$backup_parent_directory/README.md"
 
 # Individual commit message, if no parameter is set, use the current timestamp as commit message
@@ -52,7 +70,7 @@ fi
 cd "$HOME/$backup_parent_directory"
 # Check if .git exists else init git repo
 if [ ! -d ".git" ]; then
-  mkdir .git 
+  mkdir .git
   echo "[init]
           defaultBranch = $branch_name" >> .git/config #Add desired branch name to config before init
   git init
@@ -66,5 +84,11 @@ fi
 git add .
 git commit -m "$commit_message"
 git push -f -u https://"$github_token"@github.com/"$github_username"/"$github_repository".git $branch
-# Remove klipper folder after backup so that any file deletions can be logged on next backup
-rm -rf $HOME/$backup_folder/
+
+# Remove backup folder after backup so that any file deletions can be logged on next backup
+if [[ $backup_folderDepth -gt 1 ]]; then
+  rm -rf $backup_path
+  find $HOME/$backup_parent_directory -type d -empty -delete
+elif [ -d $backup_path ]; then
+    find $backup_path -maxdepth 1 -mindepth 1 ! -name '.git' -exec rm -rf {} \;
+fi
