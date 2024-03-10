f31779362681b3bc19484b3fd0a80ffda50e7e9e
MrTylerjet <mrtylerjet@gmail.com>
2024-03-10 12:00:39 -0600

    Update script.sh
    add -r (recursive) back into rsync command as it was accidentally removed when replacing old cp -r command.

--- script.sh
+++ script.sh
@@ -102,7 +102,7 @@ while IFS= read -r path; do
             elif [[ $(basename "$file") =~ ^printer-[0-9]+_[0-9]+\.cfg$ ]]; then
                 echo "Skipping file: $file"
             else
-                rsync -R "$file" "$backup_path/"
+                rsync -Rr "$file" "$backup_path/"
             fi
         done
     fi
