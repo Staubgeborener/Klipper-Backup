d9fb12cca5010d672183c081f4e6455931c761c9
MrTylerjet <MrTylerjet@gmail.com>
2024-04-16 16:09:53 -0600

    test just having a filewatch.sh script that lives in utils folder that the filewatch service calls, that way the service is always kept up to date by simply updating the .sh script and not the service file itself

---
+++ utils/filewatch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+source "$HOME/klipper-backup/.env"
+watchlist=""
+for path in "${backupPaths[@]}"; do
+    for file in $path; do
+        if [ ! -h "$file" ]; then
+            file_dir=$(dirname "$file")
+            if [ "$file_dir" = "." ]; then
+                watchlist+=" $HOME/$file"
+            else
+                watchlist+=" $HOME/$file_dir"
+            fi
+        fi
+    done
+done
+
+watchlist=$(echo "$watchlist" | tr ' ' '\n' | sort -u | tr '\n' ' ')
+
+exclude_pattern=".swp|.tmp|printer-[0-9]*_[0-9]*.cfg|.bak|.bkp"
+
+inotifywait -mrP -e close_write -e move -e delete --exclude "$exclude_pattern" $watchlist |
+while read -r path event file; do
+    if [ -z "$file" ]; then
+        file=$(basename "$path")
+    fi
+    echo "Event Type: $event, Watched Path: $path, File Name: $file"
+    file="$file" /usr/bin/env bash -c "/usr/bin/env bash  $HOME/klipper-backup/script.sh \"\$file modified - \$(date +'%%x - %%X')\"" > /dev/null 2>&1
+done
