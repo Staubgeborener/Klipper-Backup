2e24d09038b528301c2eb98489ae81d15902baa5
Staubgeborener <eric.schroeder@protonmail.com>
2024-05-02 10:35:01 +0200

    Update get-klipper-backup.sh

--- get-klipper-backup.sh
+++ get-klipper-backup.sh
@@ -1,14 +1,14 @@
 G=$'\e[1;92m' # Green
-R=$'\e[1;91m' # Red
+R=$'\e[1;91m' # Red ${R}
 NC=$'\e[0m'   # No Color
 
-if [ -d "~/klipper-backup" ]; then
+if [ -d ~/klipper-backup ]; then
     echo -e "\n${R}●${NC} Klipper-Backup directory already exists. You can run it with ~/klipper-backup/install.sh"
     exit 1
 else
     git clone https://github.com/Staubgeborener/klipper-backup.git ~/klipper-backup
     if [ $? -ne 0 ]; then
-        echo -e "\n${R}●${NC} Error when cloning the repository. Maybe Klipper-Backup already exists. You can run it with ~/klipper-backup/install.sh."
+        echo -e "\n${R}●${NC} Error when cloning the repository. Maybe Klipper-Backup already exists. You can run it with ~/klipper-backup/install.sh"
         exit 1
     fi
 
