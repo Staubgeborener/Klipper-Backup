f041a08955ae7010f510f25cbaac7af0e84f9b43
MrTylerjet <MrTylerjet@gmail.com>
2024-05-04 13:17:19 -0600

    place restore config path as a variable, delete the old .restore.config when running the script so that a fresh file is created each time to ensure any changes are properly added, add extracting the repo url to config file

--- utils/create_restore_config.sh
+++ utils/create_restore_config.sh
@@ -8,6 +8,9 @@ scriptsh_parent_path=$(
 
 source "$scriptsh_parent_path"/utils/utils.func
 source "$scriptsh_parent_path"/.env
+restore_config="$HOME"/printer_data/config/.restore.config
+
+rm -f $restore_config
 
 newbackupPaths="backupPaths=( \\ \n"
 for path in "${backupPaths[@]}"; do
@@ -15,11 +18,16 @@ for path in "${backupPaths[@]}"; do
 done
 newbackupPaths+=")"
 
-echo -e "${newbackupPaths[@]}" >"$HOME"/printer_data/config/.restore.config
+echo -e "${newbackupPaths[@]}" >>$restore_config
 
 if [ -d ""$HOME"/printer_data/config/.theme" ]; then
-    if [ "$(git remote get-url origin)" ]; then
+    if [ "$(git -C "$HOME"/printer_data/config/.theme remote get-url origin 2>/dev/null)" ]; then
         echo -e ".theme folder is a git repo"
+        loading_wheel "Extracting remote url" &
+        loading_pid=$!
+        remote_url = $(git -C "$HOME"/printer_data/config/.theme remote get-url origin)
+        echo -e $remote_url >>$restore_config
+        kill $loading_pid
     else
         echo -e ".theme is not a git repo"
     fi
