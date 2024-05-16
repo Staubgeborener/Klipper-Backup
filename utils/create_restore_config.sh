37141d9eb1f2594a69c733dc2a5c4a9fc4a556e2
MrTylerjet <MrTylerjet@gmail.com>
2024-05-15 22:24:16 -0600

    just adding some todo notes as the potential direction of the restore script is fleshed out

--- utils/create_restore_config.sh
+++ utils/create_restore_config.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#  Just some todo thoughts
+#  Should the script act independent of .env and always prompt for the token and repo you want to restore from,
+# leaning toward yes as it allows one to run restore without having to first run install.sh
+
+# 
 scriptsh_parent_path=$(
     cd "$(dirname "${BASH_SOURCE[0]}")"
     cd ..
@@ -20,6 +25,8 @@ newbackupPaths+=")"
 
 echo -e "${newbackupPaths[@]}" >>$restore_config
 
+# look at ways to determine if files are dirty from changes made after installing theme, and look at how we might back those up, 
+# maybe creating a stash and backing up the stash files only so then you fresh clone on a restore and then patch the stashed changes back in
 if [ -d ""$HOME"/printer_data/config/.theme" ]; then
     if [ "$(git -C "$HOME"/printer_data/config/.theme remote get-url origin 2>/dev/null)" ]; then
         echo -e ".theme folder is a git repo"
