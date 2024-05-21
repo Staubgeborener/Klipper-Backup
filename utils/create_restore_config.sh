faf6fe083661b75b144cf4bdb5e7648315fba17d
MrTylerjet <MrTylerjet@gmail.com>
2024-05-21 01:44:30 -0600

    wrong code syntax

--- utils/create_restore_config.sh
+++ utils/create_restore_config.sh
@@ -4,7 +4,7 @@
 #  Should the script act independent of .env and always prompt for the token and repo you want to restore from,
 # leaning toward yes as it allows one to run restore without having to first run install.sh
 
-# 
+#
 scriptsh_parent_path=$(
     cd "$(dirname "${BASH_SOURCE[0]}")"
     cd ..
@@ -25,7 +25,7 @@ newbackupPaths+=")"
 
 echo -e "${newbackupPaths[@]}" >>$restore_config
 
-# look at ways to determine if files are dirty from changes made after installing theme, and look at how we might back those up, 
+# look at ways to determine if files are dirty from changes made after installing theme, and look at how we might back those up,
 # maybe creating a stash and backing up the stash files only so then you fresh clone on a restore and then patch the stashed changes back in
 if [ -d ""$HOME"/printer_data/config/.theme" ]; then
     if [ "$(git -C "$HOME"/printer_data/config/.theme remote get-url origin 2>/dev/null)" ]; then
@@ -33,9 +33,9 @@ if [ -d ""$HOME"/printer_data/config/.theme" ]; then
         echo -e "Extracting remote url"
         remote_url=$(git -C "$HOME"/printer_data/config/.theme remote get-url origin)
         echo -e "theme_url=$remote_url" >>$restore_config
-    if [ $(git -C "$HOME"/printer_data/config/.theme git status --porcelain | grep -q '^??') ] {
-      echo $0
-    }
+        if [ $(git -C "$HOME"/printer_data/config/.theme git status --porcelain | grep -q '^??') ]; then
+            echo $0
+        fi
     else
         echo -e ".theme is not a git repo"
     fi
