dd5745ad16da94cb099087b792c5aa11c2254289
MrTylerjet <MrTylerjet@gmail.com>
2024-05-22 02:47:32 -0600

    when .theme is a repo and has untracked changes, stash the changes and pipe them into a .patch file that can be backed up to the repo for future restore actions

--- utils/create_restore_config.sh
+++ utils/create_restore_config.sh
@@ -13,7 +13,13 @@ scriptsh_parent_path=$(
 
 source "$scriptsh_parent_path"/utils/utils.func
 source "$scriptsh_parent_path"/.env
-restore_config="$HOME"/printer_data/config/.restore.config
+restore_folder="$HOME"/printer_data/config/klipper-backup-restore
+restore_config="$restore_folder"/restore.config
+theme_path="$HOME"/printer_data/config/.theme
+
+if [[ ! -d $restore_folder ]]; then
+    mkdir $restore_folder
+fi
 
 rm -f $restore_config
 
@@ -27,16 +33,25 @@ echo -e "${newbackupPaths[@]}" >>$restore_config
 
 # look at ways to determine if files are dirty from changes made after installing theme, and look at how we might back those up,
 # maybe creating a stash and backing up the stash files only so then you fresh clone on a restore and then patch the stashed changes back in
-if [ -d ""$HOME"/printer_data/config/.theme" ]; then
-    if [ "$(git -C "$HOME"/printer_data/config/.theme remote get-url origin 2>/dev/null)" ]; then
+if [ -d "$theme_path" ]; then
+    cd $theme_path
+    if [ "$(git remote get-url origin 2>/dev/null)" ]; then
         echo -e ".theme folder is a git repo"
         echo -e "Extracting remote url"
-        remote_url=$(git -C "$HOME"/printer_data/config/.theme remote get-url origin)
+        remote_url=$(git remote get-url origin)
         echo -e "theme_url=$remote_url" >>$restore_config
-        if [[ $(git -C "$HOME"/printer_data/config/.theme status --porcelain | grep '^??') ]]; then
-            echo ".theme folder has untracked changes"
+        if [[ $(git status --porcelain | grep '^??') ]]; then
+            echo ".theme folder has untracked changes. Backing up changes to .patch file"
+            git add .
+            git stash save ".theme changes"
+            git stash show -p >$restore_folder/theme_changes.patch
         fi
     else
         echo -e ".theme is not a git repo"
     fi
 fi
+
+
+#Note:
+  # use this when creating the restore script to add .theme changes back:
+  # git apply $HOME/printer_data/config/klipper-backup-restore/theme_changes.patch
