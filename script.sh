afed61d6be799dcb24ffdb65d0bea2e78b07bfaa
MrTylerjet <MrTylerjet@gmail.com>
2024-05-26 04:30:53 -0600

    still some warnings

--- script.sh
+++ script.sh
@@ -136,6 +136,7 @@ for path in "${backupPaths[@]}"; do
                 echo "Skipping .theme: $file"
             else
                 file=$(readlink -e "$file") # Get absolute path before copy (Allows usage of .. in filepath eg. ../../etc/fstab resovles to /etc/fstab )
+                echo $file
                 rsync -Rr "${file##"$HOME"/}" "$backup_path"
             fi
         done
