e1ea008a8facb7ecf5aa8aa2f27004726dc1501f
MrTylerjet <MrTylerjet@gmail.com>
2024-05-26 04:33:49 -0600

    see if this works for -name of find and delete

--- script.sh
+++ script.sh
@@ -181,4 +181,4 @@ fi
 git push -u origin "$branch_name"
 
 # Remove files except .git folder after backup so that any file deletions can be logged on next backup
-find "$backup_path" -maxdepth 1 -mindepth 1 ! -name '.git' ! -name 'README.md' -exec rm -rf {} \;
+find "$backup_path" -maxdepth 1 -mindepth 1 ! -name '.git | .gitmodules | README.md' -exec rm -rf {} \;
