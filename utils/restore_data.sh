594edea0aa0401a73d21968bdbce658392820ae5
MrTylerjet <MrTylerjet@gmail.com>
2024-06-11 21:59:31 -0600

    write out planned process for restore_data.sh

--- utils/restore_data.sh
+++ utils/restore_data.sh
@@ -1,6 +1,9 @@
-#  Just some todo thoughts
-#  Should the script act independent of .env and always prompt for the token and repo you want to restore from,
-# leaning toward yes as it allows one to run restore without having to first run install.sh
+# Planning out script process:
+# Prompt for repository name, token, branch (optional)
+# Prompt for default main branch with last commit date, as well prompt if other branches exist (branch prompt skipped if branch supplied in initial steps)
+# pull contents of branch to a temp folder, extract paths from restore.config
+# shut down instances of klipper, moonraker etc..
+# copy files from temp folder to the respective paths, along with repatching .theme git repo (if applicable)
 
 # Note:
   # use this when creating the restore script to add .theme changes back:
