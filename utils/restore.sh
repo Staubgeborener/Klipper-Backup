caa011ca8489553f234d128e81f859247c5bd405
MrTylerjet <MrTylerjet@gmail.com>
2025-02-08 11:52:07 -0700

    add some debugging

--- utils/restore.sh
+++ utils/restore.sh
@@ -75,8 +75,8 @@ configure() {
 
     getToken() {
         while true; do
-            pos=$(getcursor)
             ghtoken=$(ask_token "${C}●${NC} Enter your GitHub token associated with the backup you want to restore")
+            pos=$(getcursor)
             result=$(check_ghToken "$ghtoken") # Check GitHub Token using API
 
             if [ -n "$result" ]; then
