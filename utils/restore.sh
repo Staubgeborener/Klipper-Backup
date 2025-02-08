74d3d27310ba576cfc6ad3d9755b94f7b3b72ba2
MrTylerjet <MrTylerjet@gmail.com>
2025-02-08 11:01:38 -0700

    check if ghrepo name is empty and if so reprompt for a repo name

--- utils/restore.sh
+++ utils/restore.sh
@@ -106,9 +106,9 @@ configure() {
         pos2=$(getcursor)
         ghrepo=$(ask_textinput "${C}●${NC} Enter your repository name")
         if [ "$ghrepo" == "" ]; then
+            pos2=$(getcursor)
             tput cup $(($pos2 - 2)) 0
             tput ed
-            pos2=$(getcursor)
             echo "${Y}●${NC} Repository name cannot be empty!"
             getRepo
         fi
