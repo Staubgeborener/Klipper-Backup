0645c7083a1c4d3be7f5f5371bda545e4f927d6a
MrTylerjet <MrTylerjet@gmail.com>
2025-02-08 11:39:41 -0700

    another approach

--- utils/restore.sh
+++ utils/restore.sh
@@ -83,9 +83,10 @@ configure() {
                 ghtoken_username=$result
                 break # Exit the loop if the token is valid
             else
-                tput cup $(($pos - 2)) 0
-                #tput ed
-                echo -e "${CL}${Y}●${NC} Invalid GitHub token or unable to contact GitHub API. Please check your connection and try again!"
+                pos2=$(getcursor)
+                tput cup $(($pos2 - 2)) 0
+                tput ed
+                echo -e "${Y}●${NC} Invalid GitHub token or unable to contact GitHub API. Please check your connection and try again!"
             fi
         done
     }
