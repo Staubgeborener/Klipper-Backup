e3e730d1e466128ade1fa17b9b12672ff51b38d0
MrTylerjet <MrTylerjet@gmail.com>
2025-03-02 13:02:21 -0700

    remove loadingpid kill

--- utils/restore.sh
+++ utils/restore.sh
@@ -150,7 +150,6 @@ configure() {
             if !(git ls-tree -r HEAD --name-only | grep -q "restore.config"); then
                 tput rc
                 tput ed
-                kill $loading_pid
                 echo -e "${DM}●${NC} The latest commit for this branch does not contain the necessary files to restore. Please choose another branch or specify a commit to restore from"
                 getBranch
             fi
