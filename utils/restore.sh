a329a1bd0763f76605d5f292380032a8560fbcf8
MrTylerjet <MrTylerjet@gmail.com>
2025-03-02 14:27:17 -0700

    maybe clearUp will help

--- utils/restore.sh
+++ utils/restore.sh
@@ -152,6 +152,7 @@ configure() {
             if !(git ls-tree -r HEAD --name-only | grep -q "restore.config"); then
                 tput rc
                 clearUp
+                clearUp
                 tput ed
                 tput sc
                 echo -e "${DM}●${NC} The latest commit for this branch does not contain the necessary files to restore. Please choose another branch or specify a commit to restore from."
