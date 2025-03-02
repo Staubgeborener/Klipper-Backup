bc77028e8718815fb08fa28bee0a723768e517fe
MrTylerjet <MrTylerjet@gmail.com>
2025-03-02 14:22:05 -0700

    time to try and fix the cursor overwritting again

--- utils/restore.sh
+++ utils/restore.sh
@@ -150,7 +150,7 @@ configure() {
         else
             tempfolder
             if !(git ls-tree -r HEAD --name-only | grep -q "restore.config"); then
-                tput cup $pos 0
+                tput rc
                 tput ed
                 tput sc
                 echo -e "${DM}●${NC} The latest commit for this branch does not contain the necessary files to restore. Please choose another branch or specify a commit to restore from"
