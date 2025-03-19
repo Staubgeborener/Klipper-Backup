7e04380743524d741bf9639984a596c829ab9b3b
MrTylerjet <MrTylerjet@gmail.com>
2025-03-19 01:44:30 -0600

    forgot to save after re-adding logo back

--- utils/restore.sh
+++ utils/restore.sh
@@ -24,6 +24,7 @@ main() {
     dependencies
     check_klipper_installed
     configure
+    logo
     copyRestoreConfig
     source $temprestore
     sudo systemctl stop klipper.service
@@ -31,6 +32,21 @@ main() {
     restoreMoonrakerDB
     copyTheme
     cleanup
+}
+
+logo() {
+    clear
+    echo -e "${C}$(
+        cat <<"EOF"
+    __ __ ___                             ____             __                     ____            __                
+   / //_// (_)___  ____  ___  _____      / __ )____ ______/ /____  ______        / __ \___  _____/ /_____  ________ 
+  / ,<  / / / __ \/ __ \/ _ \/ ___/_____/ __  / __ `/ ___/ //_/ / / / __ \______/ /_/ / _ \/ ___/ __/ __ \/ ___/ _ \
+ / /| |/ / / /_/ / /_/ /  __/ /  /_____/ /_/ / /_/ / /__/ ,< / /_/ / /_/ /_____/ _, _/  __(__  ) /_/ /_/ / /  /  __/
+/_/ |_/_/_/ .___/ .___/\___/_/        /_____/\__,_/\___/_/|_|\__,_/ .___/     /_/ |_|\___/____/\__/\____/_/   \___/ 
+         /_/   /_/                                               /_/                                                
+EOF
+    )${NC}"
+    line
 }
 
 check_klipper_installed() {
