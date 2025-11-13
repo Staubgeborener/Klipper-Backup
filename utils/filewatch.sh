a32341fad110871a052b2b4370d345e6be9a6721
Staubgeborener <eric.schroeder@protonmail.com>
2025-11-13 20:05:57 +0100

    Refactor filewatch.sh to check printer status

--- utils/filewatch.sh
+++ utils/filewatch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+ 
 source "$HOME/klipper-backup/.env"
 watchlist=""
 for path in "${backupPaths[@]}"; do
@@ -14,16 +14,20 @@ for path in "${backupPaths[@]}"; do
         fi
     done
 done
-
+ 
 watchlist=$(echo "$watchlist" | tr ' ' '\n' | sort -u | tr '\n' ' ')
-
+ 
 exclude_pattern=".swp|.tmp|printer-[0-9]*_[0-9]*.cfg|.bak|.bkp"
-
+ 
 inotifywait -mrP -e close_write -e move -e delete --exclude "$exclude_pattern" $watchlist |
 while read -r path event file; do
     if [ -z "$file" ]; then
         file=$(basename "$path")
     fi
-    echo "Event Type: $event, Watched Path: $path, File Name: $file"
-    file="$file" /usr/bin/env bash -c "/usr/bin/env bash  $HOME/klipper-backup/script.sh -c \"\$file modified - \$(date +'%x - %X')\"" > /dev/null 2>&1
+ 
+    status=$(curl -s http://localhost:7125/printer/objects/query?print_stats | jq -r '.result.status.print_stats.state // empty')
+    if [ "$status" != "printing" ]; then
+        echo "Event Type: $event, Watched Path: $path, File Name: $file"
+        file="$file" /usr/bin/env bash -c "/usr/bin/env bash $HOME/klipper-backup/script.sh -c \"\$file modified - \$(date +'%x - %X')\"" > /dev/null 2>&1
+    fi
 done
